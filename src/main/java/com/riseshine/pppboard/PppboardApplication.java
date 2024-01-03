5af01dbafd9c2d0f24d1870460ffc9fedb89bea1
seungheee <lsh3431@gmail.com>
2024-01-03 21:03:42 +0900

    init

---
+++ src/main/java/com/riseshine/pppboard/PppboardApplication.java
@@ -0,0 +1,13 @@
+package com.riseshine.pppboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PppboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PppboardApplication.class, args);
+	}
+
+}
