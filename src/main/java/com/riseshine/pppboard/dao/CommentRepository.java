fa1992b127c7b475d497d0506de1ad3aefd1714c
seungheee <lsh3431@gmail.com>
2024-02-06 20:05:04 +0900

    [PPP-011] feat: 댓글 생성
    - 댓글 생성
    - 자식 댓글 생성

--- src/main/java/com/riseshine/pppboard/dao/CommentRepository.java
+++ src/main/java/com/riseshine/pppboard/dao/CommentRepository.java
@@ -1,8 +1,12 @@
 package com.riseshine.pppboard.dao;
 
 import com.riseshine.pppboard.domain.Comment;
+import com.riseshine.pppboard.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer>, JpaSpecificationExecutor<Comment> {
+  Optional<Comment> findFirstByNo(int no);
 }
